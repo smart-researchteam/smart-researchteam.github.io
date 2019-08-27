c80d574609477dde9d62600da09bed4e83df04a8
Jean-Michel Bruel <jbruel@gmail.com>
2019-08-27 17:26:56 +0200

    rebuilding site Mar 27 aoû 2019 17:26:56 CEST

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
 # info for the rsync IRIT
 USER=bruel
 HOST=ftpintra.irit.fr
 DIR=/usr/local/WWW2/Equipes/SMART/site/   # might sometimes be empty!
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mBuilding pages...\033[0m"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+echo -e "\033[0;32mDeploying updates to IRIT...\033[0m"
+
 # Deploy @ IRIT.
 rsync -avz --delete public/ ${USER}@${HOST}:${DIR}
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Go To Public folder
 cd public
