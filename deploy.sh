af27cbb75f6b1438b649c3f4cacb98f63104bc54
JMB <jbruel@gmail.com>
2021-12-15 17:45:05 +0100

    📦 NEW: Update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # info for the rsync IRIT
-USER=ebersold
-HOST=ftpintra.irit.fr
+USER=bruel
+#HOST=ftpintra.irit.fr
+HOST=bali.irit.fr
 DIR=/usr/local/WWW2/Equipes   # might sometimes be empty!
 
 echo -e "\033[0;32mBuilding pages...\033[0m"
@@ -13,6 +14,8 @@ hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 echo -e "\033[0;32mDeploying updates to IRIT...\033[0m"
 
 # Deploy @ IRIT.
-rsync -avz --protocol=29 --delete public/ ${USER}@${HOST}:${DIR}
+# rsync -avz --protocol=29 --delete public/ ${USER}@${HOST}:${DIR}
+rsync -avz -e 'ssh -o "ProxyCommand ssh -A ${USER}@sash.irit.fr -W %h:%p"' public/ ${USER}@${HOST}:${DIR}
+
 
 exit 0
