7a955dbc9bb942c11385bdaae77c7e1e152981b6
Jean-Michel Bruel <jbruel@gmail.com>
2025-04-15 17:35:47 +0200

    🐛 FIX: Redirecting IRIT web site because of CSS issues

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ DIR=/usr/local/WWW2/Equipes/SMART/site   # might sometimes be empty!
 echo -e "\033[0;32mBuilding pages...\033[0m"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 echo -e "\033[0;32mDeploying updates to IRIT...\033[0m"
 
